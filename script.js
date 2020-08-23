28d72fd11f5ba89b0e37e9a710d3fa6a5ce7dccc
Lígia Menezes <58963430+ligiammenezes@users.noreply.github.com>
2020-08-23 15:26:40 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,27 @@
+const modalOverlay=document.querySelector(".modal-overlay")
+const modal=document.querySelector(".modal")
+const cards=document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener("click",function(){
+        const cardID= card.getAttribute("id");
+        modalOverlay.classList.add('active')
+    
+        modalOverlay.querySelector("iframe").src=`https://rocketseat.com.br/${cardID}`
+    })
+}
+
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+    modal.classList.remove('maximize')
+    modalOverlay.querySelector("iframe").src=""
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    if(modal.classList.contains("maximize")){
+        modal.classList.remove("maximize")
+    }else {
+        modal.classList.add("maximize")
+    }
+})
